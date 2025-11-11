9badc33b3c3761811a84f81e97deb818edee5fa9
Gunaranjan <gunaachar003@example.com>
2025-11-11 11:37:07 +0000

    edited this file and added new line to it

--- docker_alert.sh
+++ docker_alert.sh
@@ -5,4 +5,4 @@ docker_version=$(docker --version | awk '{print $3}' | tr -d ',')
 current_date=$(date)
 
 echo -e "Docker_status=$docker_status\nDocker_version=$docker_version" | mail -s "test email" gunaachar003@gmail.com
-
+echo "This is sample file"
