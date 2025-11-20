59c24aa82ae76ef39ed16a4195afb1928c41f086
GunaranjanV <gunaachar003@gmail.com>
2025-11-20 21:41:50 +0530

    added jenkinsfile

--- docker_alert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker_status=$(systemctl is-active docker)
-docker_version=$(docker --version | awk '{print $3}' | tr -d ',')
-current_date=$(date)
-
-echo -e "Docker_status=$docker_status\nDocker_version=$docker_version" | mail -s "test email" gunaachar003@gmail.com
-echo "This is sample file"
